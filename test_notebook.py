e36d76d82c8c6ccced14458e89be5307b107bc72
Adamovich <Oleg.Adamovich@bp.com>
2020-05-09 10:39:41 +0300

    Test Jupiter book commitemnt_2

---
+++ test_notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for Capstone Data Science project
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
